43eb6a367e85b7b5dbe1ca561d1570060b6568f9
Indra Sah Noeldy <146718520+indra87g@users.noreply.github.com>
2024-04-08 17:01:36 +0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,5 +1,8 @@
 import { numerical, convert } from "./index";
+import Benchmark from "benchmark";
+import { withCodSpeed } from "@codspeed/benchmark.js-plugin";
 
+const suite = withCodSpeed(new Benchmark.Suite());
 numerical.Exp(9);
 numerical.Mult(9, 8);
 numerical.Squ(81);
